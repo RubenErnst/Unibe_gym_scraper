10629df163b9fc610da35b75d794636a9068348a
RubenErnst <ruben.ernst@bluewin.ch>
2022-11-16 08:48:57 +0100

    Add error handling

--- scraper.R
+++ scraper.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(RSelenium)
 
 
-chromeDr <- rsDriver(browser = "chrome", port = 4567L, chromever = "106.0.5249.61", # you will have to adjust this version
+chromeDr <- rsDriver(browser = "chrome", port = 4570L, chromever = "106.0.5249.61", # you will have to adjust this version
                      extraCapabilities = list(chromeOptions = list(args = c('--disable-gpu', '--window-size=1920,1080', '--headless'),
                                                                    prefs = list(
                                                                      "profile.default_content_settings.popups" = 0L,
@@ -14,24 +14,94 @@ chromeDr <- rsDriver(browser = "chrome", port = 4567L, chromever = "106.0.5249.6
 
 remDr <- chromeDr[["client"]]
 
+url <- "https://www.unibe.ch/universitaet/campus__und__infrastruktur/universitaetssport/sportangebot/fitnessraeume/index_ger.html"
+
 # start
-unibe_gym <- data.frame()
+load(file = "unibe_gym.RData")
+counter <- 0
 
 while(TRUE){
-  remDr$navigate("https://www.unibe.ch/universitaet/campus__und__infrastruktur/universitaetssport/sportangebot/fitnessraeume/index_ger.html")
-  Sys.sleep(5)
+  if (counter > 10){
+    remDr$close()
+    chromeDr[["server"]]$stop()
+    
+    chromeDr <- rsDriver(browser = "chrome", port = 4567L, chromever = "106.0.5249.61", # you will have to adjust this version
+                         extraCapabilities = list(chromeOptions = list(args = c('--disable-gpu', '--window-size=1920,1080', '--headless'),
+                                                                       prefs = list(
+                                                                         "profile.default_content_settings.popups" = 0L,
+                                                                         "download.prompt_for_download" = FALSE,
+                                                                         "directory_upgrade" = TRUE
+                                                                       ))))
+    
+    remDr <- chromeDr[["client"]]
+    counter <- 0
+  }
   
-  e <- remDr$findElement(value = "//*/div[contains(@class, 'go-stop-display_footer')]")
-  sign_color <- unlist(str_split(unlist(str_split(e$getElementAttribute("class"), " "))[4], "_"))[2]
-  occupants <- as.numeric(unlist(str_split(unlist(e$getElementText()), " von "))[1])
+  error_occ <- TRUE
+  tryCatch(
+    expr = {
+      suppressMessages({
+        remDr$navigate(url)
+        Sys.sleep(5)
+        error_occ <- FALSE
+      })
+    },
+    error = function(err){
+      return(1)
+    }
+  )
+  
+  if (error_occ){
+    remDr$close()
+    chromeDr[["server"]]$stop()
+    
+    chromeDr <- rsDriver(browser = "chrome", port = 4567L, chromever = "106.0.5249.61", # you will have to adjust this version
+                         extraCapabilities = list(chromeOptions = list(args = c('--disable-gpu', '--window-size=1920,1080', '--headless'),
+                                                                       prefs = list(
+                                                                         "profile.default_content_settings.popups" = 0L,
+                                                                         "download.prompt_for_download" = FALSE,
+                                                                         "directory_upgrade" = TRUE
+                                                                       ))))
+    
+    remDr <- chromeDr[["client"]]
+    counter <- 0
+    
+    tryCatch(
+      expr = {
+        suppressMessages({
+          remDr$navigate(url)
+          Sys.sleep(5)
+        })
+      },
+      error = function(err){
+        return(1)
+      }
+    )
+  }
+  
+  sign_color <- NA
+  occupants <- NA
+  
+  tryCatch(
+    expr = {
+      suppressMessages({
+        e <- remDr$findElement(value = "//*/div[contains(@class, 'go-stop-display_footer')]")
+        sign_color <- unlist(str_split(unlist(str_split(e$getElementAttribute("class"), " "))[4], "_"))[2]
+        occupants <- as.numeric(unlist(str_split(unlist(e$getElementText()), " von "))[1])
+      })
+    },
+    error = function(err){
+      return(1)
+    })
   
   unibe_gym <- rbind(unibe_gym, data.frame("timestamp" = Sys.time(),
                                            "sign_color" = sign_color,
                                            "occupants" = occupants))
   
-  print(paste0("Successful snapshot at ", Sys.time()))
+  print(paste0("Took snapshot at ", Sys.time()))
   
   save(unibe_gym, file = "unibe_gym.RData")
+  counter <- counter + 1
   Sys.sleep(294)
 }
 
